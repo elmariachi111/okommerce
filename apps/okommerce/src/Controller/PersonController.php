027e37fe00d984a452b0e9757f03a2bca757205b
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-12-03 19:35:37 +0100

    downgraded php -> no wide attrs support adds composer step in php docker file adds Product entity

--- apps/okommerce/src/Controller/PersonController.php
+++ apps/okommerce/src/Controller/PersonController.php
@@ -9,7 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PersonController extends AbstractController
 {
-    #[Route('/', name: 'action')]
+    /**
+     * @Route("/", name="action")
+     */
     public function index(): Response
     {
         $number = random_int(0, 1000);
---
+++ apps/okommerce/src/Entity/Product.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="guid")
+     */
+    private $uid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sku;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUid(): ?string
+    {
+        return $this->uid;
+    }
+
+    public function setUid(string $uid): self
+    {
+        $this->uid = $uid;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
